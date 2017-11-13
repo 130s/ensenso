c00375cf2dc0b6064727debd9382ac0a455f9e20
Isaac I.Y. Saito <130s@2000.jukuin.keio.ac.jp>
2017-11-13 10:36:25 -0800

    [doc][CI] Refactoring.

--- scripts/install_driver.sh
+++ scripts/install_driver.sh
@@ -1,6 +1,24 @@
 #!/bin/sh
+
+# Copyright (c) 2017, PlusOne Robotics Inc.
+# Copyright (c) 2017, CRI Group, Singapore.
+# All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Description
 # Downloading proprietary deb files from https://www.ensenso.com/support/sdk-download, and install them.
+# Lots of logics in this file are specific to Docker. When used on a local host, you will need sudo, but won't need some tweeks.
 
 # Update the URL when newer versions available.
 URL_CODEMETER=https://download.ensenso.com/s/ensensosdk/download?files=codemeter_6.40.2402.501_amd64.deb
